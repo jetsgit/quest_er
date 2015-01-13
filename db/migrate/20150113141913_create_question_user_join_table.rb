ac5b01e9ab9d5998a87d77d9d0d50d390ff857a5
Jerrold Thompson <jet@whidbey.com>
2015-01-13 06:26:37 -0800

    run migrations for user, question, join table

--- db/migrate/20150113141913_create_question_user_join_table.rb
+++ db/migrate/20150113141913_create_question_user_join_table.rb
@@ -3,5 +3,6 @@ class CreateQuestionUserJoinTable < ActiveRecord::Migration
     create_table :questions_users, id: false do |t|
       t.belongs_to :question, index: true
       t.belongs_to :user, index: true
+    end
   end
 end
