697b13133f79ba0b2d2c3a8a7839f22e7295730a
Jerrold Thompson <jet@whidbey.com>
2015-01-13 06:32:52 -0800

    habtm added in question/user

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,4 @@
 class Question < ActiveRecord::Base
+  has_and_belongs_to_many :users, join_table: 'questions_users', foreign_key: 'question_id'
+  before_action :authenticate_user!
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_and_belongs_to_many :questions, join_table: 'questions_users', foreign_key: 'user_id'
 end
